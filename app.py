4bc42787e8460925ac86670c02918190b34a9335
MFBenedetto <mbenedetto@gmail.com>
2023-05-04 23:38:15 +0200

    Second commit

---
+++ app.py
@@ -0,0 +1,53 @@
+"""Streamlit app to generate Tweets."""
+
+# Import from standard library
+import logging
+import random
+import re
+
+# Import from 3rd party libraries
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Import modules
+import oai
+
+# Configure logger
+logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, force=True)
+
+
+# Configure Streamlit page and state
+st.set_page_config(page_title="Tweet", page_icon="📚")
+
+# Force responsive layout for columns also on mobile
+st.write(
+    """<style>
+    [data-testid="column"] {
+        width: calc(50% - 1rem);
+        flex: 1 1 calc(50% - 1rem);
+        min-width: calc(50% - 1rem);
+    }
+    </style>""",
+    unsafe_allow_html=True,
+)
+
+# Render Streamlit page
+st.title("Tell me a story!")
+st.markdown(
+    "Generate an unlimited number of stories with this app!
+)
+
+st.markdown("""---""")
+
+st.write("If you like this app, please consider")
+    components.html(
+        """
+            <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id"/>
+            <input type="image" src="https://pics.paypal.com/00/s/MDY0MzZhODAtNGI0MC00ZmU5LWI3ODYtZTY5YTcxOTNlMjRm/file.PNG" height="35" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+            </form>
+        """,
+        height=45,
+    )
+st.write("so I can keep it alive. Thank you!")
