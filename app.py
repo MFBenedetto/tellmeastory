cdfdebda907adf210781ae56545d9435352612ab
MFBenedetto <mbenedetto@gmail.com>
2023-05-16 22:21:56 +0200

    update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ logging.basicConfig(format="\n%(asctime)s\n%(message)s", level=logging.INFO, for
 
 
 # Configure Streamlit page and state
-st.set_page_config(page_title="Tweet", page_icon="📚")
+st.set_page_config(page_title="Tell me a story", page_icon="📚")
 
 # Force responsive layout for columns also on mobile
 st.write(
@@ -34,20 +34,20 @@ st.write(
 # Render Streamlit page
 st.title("Tell me a story!")
 st.markdown(
-    "Generate an unlimited number of stories with this app!
+    "Generate an unlimited number of stories with this app!"
 )
 
 st.markdown("""---""")
 
 st.write("If you like this app, please consider")
-    components.html(
-        """
-            <form action="https://www.paypal.com/donate" method="post" target="_top">
-            <input type="hidden" name="hosted_button_id"/>
-            <input type="image" src="https://pics.paypal.com/00/s/MDY0MzZhODAtNGI0MC00ZmU5LWI3ODYtZTY5YTcxOTNlMjRm/file.PNG" height="35" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
-            <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-            </form>
-        """,
-        height=45,
-    )
+components.html(
+    """
+        <form action="https://www.paypal.com/donate" method="post" target="_top">
+        <input type="hidden" name="hosted_button_id"/>
+        <input type="image" src="https://pics.paypal.com/00/s/MDY0MzZhODAtNGI0MC00ZmU5LWI3ODYtZTY5YTcxOTNlMjRm/file.PNG" height="35" border="0" name="submit" title="Donate with PayPal" alt="Donate with PayPal button" />
+        <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+    """,
+    height=45,
+)
 st.write("so I can keep it alive. Thank you!")
